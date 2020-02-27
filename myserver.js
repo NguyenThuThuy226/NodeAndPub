d76210a0ae3e7d547756ddf3e7400dacf30d76a1
NguyenThuThuy226 <Thuynt@sonhaisq.vn>
2020-02-27 16:10:44 +0700

    new code to learn

--- myserver.js
+++ myserver.js
@@ -7,9 +7,24 @@ var port = process.env.PORT || 3000;
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
+var bodyParser= require('body-Parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 //khi mo website mac dinh
 app.get("/",(req,res)=>{
     res.render('index');
+})
+
+app.get('/form',(req,res)=>{
+    res.render('form');
+})
+
+app.post('/saveForm',(req,res)=>{
+    //lay thong tin tu textbox ten la yourName
+    const yourName = req.body.yourName;
+    res.render('confirm',{name:yourName});
+    //console.log(yourName);
+
 })
 
 var server=app.listen(port, function() {});
